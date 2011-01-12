223982f25d0e8c6c52a2086250c3cc5c41a1ee97
Maxwell Dayvson <dayvson@gmail.com>
2011-01-12 06:22:36 -0200

    Ajustes no controller da home

---
+++ bookmarks.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var sys = require('sys');
+var app = express.createServer();
+var HomeController = require('./controllers/home').HomeController;
+app.configure(function() {
+    app.use(express.logger());
+    app.use(express.bodyDecoder());
+    app.use(express.methodOverride());
+    app.set('view engine', 'ejs');
+    app.set('views', __dirname + '/views');
+    app.set('partials'   , __dirname + '/views/partials');
+    app.use(express.staticProvider(__dirname + '/public'));
+    app.set('view options', {
+        layout: 'layout'
+    });
+});
+
+//Rotas de controle
+app.get("/", HomeController.index);
+app.get('/search', HomeController.search);
+app.get("/list", HomeController.listAll);
+app.put("/add/create", HomeController.addNew);
+app.get("/add", HomeController.add);
+app.get("/remove/:id", HomeController.remove);
+
+//Inicializa o Servidor
+app.listen(3000);
+console.log("Server on port %s", app.address().port);
